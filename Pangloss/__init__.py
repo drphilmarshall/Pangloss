87be63b65311ccd7cf959b6c002b748aa7e9fd25
Thomas Collett <tcollett@calx103.ast.cam.ac.uk>
2012-09-17 15:54:23 +0100

    edit draft

--- Pangloss/__init__.py
+++
@@ -1,9 +0,0 @@
-from lightcone import *
-from kappamap import *
-from grid import *
-from LensingFunc import *
-from LensingProfiles import *
-import Relations as Rel
-from smooth import *
-
-#from lightcone_reconstruction import *
