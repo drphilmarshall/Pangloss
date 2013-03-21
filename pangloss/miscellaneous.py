ec2f6b46c1b1d24092bf582b99da528e6165e56b
Phil Marshall <dr.phil.marshall@gmail.com>
2013-03-21 22:21:41 +0000

    stdout, constants

---
+++ pangloss/miscellaneous.py
@@ -0,0 +1,10 @@
+
+import numpy
+
+arcmin2rad = (1.0/60.0)*numpy.pi/180.0
+rad2arcmin = 1.0/arcmin2rad
+
+dashedline = '--------------------------------------------------------------------------------'
+doubledashedline = '================================================================================'
+
+hello = '              Pangloss: reconstructing all the mass in the Universe             '
