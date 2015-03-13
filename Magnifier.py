8fcbf0ed7ac320dcb4a8f4f0ecb63ede9058e845
Charlotte Mason <charlotte.nosam@gmail.com>
2015-03-13 12:15:48 -0700

    finished magnifier

--- Magnifier.py
+++ Magnifier.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # ======================================================================
-from matplotlib import rc_file
-rc_file('matplotlibrc')
-
 import pangloss
 
 import sys,getopt,cPickle,numpy,glob
@@ -22,40 +19,49 @@ def Magnifier(argv):
 
     PURPOSE
         Read in a simulation lightcone (or list of lightcones) and compute all 
-        quantities needed to estimate muh, the convergence due to
-        halos, at the centre of the lightcone.
-        Output is a list of muh values drawn from Pr(muh|D), 
-        where D refers to either observed data, or simulated data from 
-        a calibration line of sight.
+        quantities needed to estimate mu, the magnification along the LoS to a 
+        background source. This file create p(mu) for all lines of sight, and
+        also p(mu) for observed fields which are matched by overdensity (or 
+        simply for LoS with a given overdensity, not necessarily real ones)
+
+        Requires extra inputs in the config file, detailed below
 
     COMMENTS
         The config file contains the list of lightcones to be
         reconstructed, in the form of either a directory or a single
         instance. If a directory is specified, one also gives a number
         ("batchsize") of lightcones to be reconstructed at a time. 
-        The number of kappah samples desired must also be given in the
-        config file.
 
     FLAGS
-        -h            Print this message [0]
+        -h                  Print this message [0]
+        -c, --contributions Plot cumulative contributions
 
     INPUTS
         configfile    Plain text file containing Pangloss configuration
+
+        additional configfile fields:
+        FieldName           Plain text or csv file containing list of field
+                            names, can be None
+        FieldOverdensity    Plain text or csv file containing list of field
+                            overdensities. Must be in same order as field 
+                            names, can be None
 
     OUTPUTS
         stdout        Useful information
         samples       Catalog(s) of samples from Pr(kappah|D)
 
     EXAMPLE
-        Magnifier.py example.config --plot_contributions
+        Magnifier.py --contributions example.config 
 
     BUGS
-        - Code is incomplete.
+        Plotting contributions is SLOW
 
     AUTHORS
       This file is part of the Pangloss project, distributed under the
-      GPL v2, by Tom Collett (IoA) and  Phil Marshall (Oxford). 
-      Please cite: Collett et al 2013, http://arxiv.org/abs/1303.6564
+      GPL v2, by Tom Collett (IoA) and  Phil Marshall (Oxford). This 
+      Magnifier code was developed by Charlotte Mason (UCSB)
+      Please cite: Collett et al. 2013, http://arxiv.org/abs/1303.6564
+                   Mason et al. 2015, http://arxiv.org/abs/1502.03795
 
     HISTORY
       2013-03-21 started Collett & Marshall (Oxford)
@@ -65,7 +71,7 @@ def Magnifier(argv):
     # --------------------------------------------------------------------
 
     try:
-       opts, args = getopt.getopt(argv,"h",["help"])
+       opts, args = getopt.getopt(argv,"hc",["help","contributions"])
     except getopt.GetoptError, err:
        print str(err) # will print something like "option -a not recognized"
        print Magnifier.__doc__  # will print the big comment above.
@@ -73,23 +79,27 @@ def Magnifier(argv):
 
     for o,a in opts:
        if o in ("-h", "--help"):
+          print "HELP!"
           print Magnifier.__doc__
           return
+       elif o in ("-c", "--contributions"):
+          print "Magnifier: plotting cumulative contributions"
+          plot_contributions = True
        else:
           assert False, "unhandled option"
 
     # Check for setup file in array args:
+    print args
+    print opts
     if len(args) == 1:
         configfile = args[0]
-    elif len(args) == 2:
-        configfile = args[0]
-        plot_contributions == args[]
         print pangloss.doubledashedline
         print pangloss.hello
         print pangloss.doubledashedline
         print "Magnifier: generating magnification PDFs for lightcones of given density"
         print "Magnifier: taking instructions from",configfile
     else:
+        print "Magnifier: are you sure you put the options before the config file?"
         print Magnifier.__doc__
         return
 
@@ -110,7 +120,10 @@ def Magnifier(argv):
     field_name = experiment.parameters['FieldName']
     field_overdensity = experiment.parameters['FieldOverdensity']
 
-    field_name = numpy.genfromtxt(str(field_name), comments='#', usecols=0, dtype='S30')
+    if field_name == None:
+        field_name = ''
+    else:
+        field_name = numpy.genfromtxt(str(field_name), comments='#', usecols=0, dtype='S30')
     if len(field_overdensity)==1: field_overdensity = numpy.genfromtxt(str(field_overdensity), comments='#')
     
     Rc = experiment.parameters['LightconeRadius'] # in arcmin
@@ -255,7 +268,7 @@ def Magnifier(argv):
             lc.plots('kappa', output=CALIB_DIR+"/example_snapshot_kappa_uncalib_z=1.4.png")
             lc.plots('mu', output=CALIB_DIR+"/example_snapshot_mu_uncalibz=1.4.png")
        
-       del lc
+        del lc
        
     pk = numpy.array(pk)
     pmu = numpy.array(pmu)    
@@ -268,6 +281,8 @@ def Magnifier(argv):
 
     del pk
     del pmu
+
+    print "Magnifier: saved PofMu to "+CALIB_DIR+"/Pofk_z="+str(zs)+".pickle"
 
     pmu = pangloss.readPickle(CALIB_DIR+"/PofMu_z="+str(zs)+".pickle")
     pk = pangloss.readPickle(CALIB_DIR+"/Pofk_z="+str(zs)+".pickle")
@@ -294,27 +309,18 @@ def Magnifier(argv):
         plt.tight_layout()
         plt.xlabel('Redshift, z')
         plt.savefig("figs/"+EXP_NAME+"contribution_z.pdf",dpi=300)
- 
-    # ==============================================================
-    # Plot the pdfs
-    # ==============================================================
-    
+
+
     # --------------------------------------------------------------------
     # Calculate the smooth components
     kappa_smooth = numpy.mean(pk)
     mu_smooth = numpy.mean(pmu)
-    print 'Uncalibrated: <kappa> =',kappa_smooth, '<mu> =',mu_smooth
+    print '           uncalibrated: <kappa> =',kappa_smooth, '<mu> =',mu_smooth
 
     pmu = pmu - mu_smooth + 1.
     pk = pk - kappa_smooth + 0.
-    print 'mean mu now = ', numpy.mean(pmu)
-    # --------------------------------------------------------------------
-    # Plotting convergence and magnification
-    c = ['purple','g','r','b']
-    
-#    params = [{'param':'Kappa', 'name':r'$\kappa$', 'lc':pk, 'smooth':kappa_smooth, 'mean':0.0, 'height':60, 'min':-0.2, 'max':0.4},
-#            {'param':'Mu', 'name':r'$\mu$', 'lc':pmu, 'smooth':mu_smooth, 'mean':1.0, 'height':30, 'min':0.4, 'max':2.0}]
-   
+    print '           mean mu now = ', numpy.mean(pmu)
+
     params = [{'param':'Mu', 'name':r'$\mu$', 'lc':pmu, 'smooth':mu_smooth, 'mean':1.0, 'height':30, 'min':0.4, 'max':2.0}]
     
     lc_density = numpy.genfromtxt(CALIB_DIR+"/lc_density.txt", comments="#")
@@ -322,138 +328,78 @@ def Magnifier(argv):
     # =====================================================================
     # For only <=4 values of density
     # =====================================================================    
-    if len(density) < 5:
-        for k in range(len(params)):
+    for k in range(len(params)):
+
+        var = params[k]
+        full_pdf = var['lc']
+        name = var['name']     
+        print 'Magnifier: Old min and max:', full_pdf.min(), full_pdf.max()
         
-            var = params[k]
-            full_pdf = var['lc']
-            name = var['name']     
-            print 'Magnifier: Old min and max:', full_pdf.min(), full_pdf.max()
-            
-            # --------------------------------------------------------------------
-            # Remove outliers --  only for v high overdensity?
-            if var['param']=='Kappa':
-                mask = numpy.where(full_pdf==full_pdf)
-            if var['param']=='Mu':
-                mask = numpy.where((full_pdf > var['smooth']-5.) & (full_pdf < 6.+var['smooth']))
-                
-            par = full_pdf[mask]
-            new_density = lc_density[mask]
-            
-            print '         Removing outliers...'
-            print '         New min and max:', par.min(), par.max()
-            
-            # Recalculate means and the calibrated pdf
-            smooth_new = numpy.mean(par) 
-            new_pdf = par - smooth_new + var['mean']
-            par_mean = numpy.mean(new_pdf)
-            print 'New mean (this should be',var['mean'],'):', par_mean         
+        # --------------------------------------------------------------------
+        # Remove outliers  !!! I think this is only important for v. high overdensity
+        if var['param']=='Kappa':
+            mask = numpy.where(full_pdf==full_pdf)
+        if var['param']=='Mu':
+            mask = numpy.where((full_pdf >= 0.) & (full_pdf < 2.))
+        
+        par = full_pdf[mask]
+        new_density = lc_density[mask]
 
-            # --------------------------------------------------------------------
-            # Plot PDF for all lines of sight
-            
-            print pangloss.dashedline
-            print "Magnifier: constructing PDF for", var['param'],"..."  
-            
-            # --------------------------------------------------------------------
-            # Select only lightcones within certain number density limits
-            # Need to mask out the outliers
-            
-            for i in range(len(density)):
-                sub_pdf = [] 
+        print '           Removing Outliers...'
+        print '           New min and max:', par.min(), par.max()
+        
+        # Recalculate means and the calibrated pdf
+        smooth_new = numpy.mean(par) 
+        new_pdf = par - smooth_new + var['mean']
+        par_mean = numpy.mean(new_pdf)
+        print '           New mean (this should be',var['mean'],'):', par_mean        
+        
+        # --------------------------------------------------------------------
+        # Plot all lines of sight
+        
+        print pangloss.dashedline
+        print "Magnifier: constructing PDF for", var['param'],"..."  
                 
-                for j in xrange(len(new_pdf)):
-                    if density[i] - drange <= round(new_density[j],2) <= density[i] + drange:
-                        sub_pdf.append(new_pdf[j])
-                
-                sub_pdf = numpy.array(sub_pdf)
+        outputfile = CALIB_DIR+"/"+EXP_NAME+"_Pof"+var['param']+"_"+"_z="+str(zs)+"_allLoS.txt"                 
+        numpy.savetxt(outputfile, sub_pdf) 
+        print "Magnifier: saved all LoS PDFs to",outputfile
+                                                  
+        # --------------------------------------------------------------------
+        # Select only lightcones within certain number density limits
+        # Need to mask out the outliers
+        
+        means, fieldname = [], []
+        
+        for i in range(len(density)):
+            sub_pdf = [] 
+            
+            for j in xrange(len(new_pdf)):
+                if density[i] - drange <= round(new_density[j],2)  <= density[i] + drange:
+                    sub_pdf.append(new_pdf[j])
+            
+            sub_pdf = numpy.array(sub_pdf)
+            Nlos = len(sub_pdf)
+            if Nlos == 0:
+                print "Magnifier: %s - there are NO LoS with number density ~ %.2f the average" % (field_name[i], density[i])    
+                print "Magnifier: %s - no PDF will be made for this field" % (field_name[i])    
+            
+            else:
+
                 sub_mean = numpy.mean(sub_pdf) 
-                Nlos = len(sub_pdf)
                             
-                print "Magnifier: sampling %i LoS with number density ~ %.2f the average" % (Nlos, density[i])   
-            
-                outputfile = CALIB_DIR+"/"+EXP_NAME+"_Pof"+var['param']+"_"+"_z="+str(zs)+"_overdensity="+str(density[i])+".txt"                 
-                numpy.savetxt(outputfile, sub_pdf) 
-                print "Magnifier: saved PDFs to",outputfile
-
-    # =====================================================================
-    # For many values of overdensity e.g. BORG
-    # =====================================================================    
-
-    else:
-        for k in range(len(params)):
-      
-            var = params[k]
-            full_pdf = var['lc']  #- var['smooth'] + var['mean']
-            name = var['name']     
-            print 'Magnifier: Old min and max:', full_pdf.min(), full_pdf.max()
-            
-            # --------------------------------------------------------------------
-            # Remove outliers  !!! I think this is only important for v. high overdensity
-            if var['param']=='Kappa':
-                mask = numpy.where(full_pdf==full_pdf)
-            if var['param']=='Mu':
-                mask = numpy.where((full_pdf >= 0.) & (full_pdf < 2.))
-#                mask = numpy.where((full_pdf > var['smooth']-5.) & (full_pdf < 6.+var['smooth']))
-            
-            par = full_pdf[mask]
-            new_density = lc_density[mask]
-
-            print '         Removing Outliers...'
-            print '         New min and max:', par.min(), par.max()
-            
-            # Recalculate means and the calibrated pdf
-            smooth_new = numpy.mean(par) 
-            new_pdf = par - smooth_new + var['mean']
-            par_mean = numpy.mean(new_pdf)
-            print 'New mean (this should be',var['mean'],'):', par_mean        
-            
-            # --------------------------------------------------------------------
-            # Plot all lines of sight
-            
-            print pangloss.dashedline
-            print "Magnifier: constructing PDF for", var['param'],"..."  
-                    
-            outputfile = CALIB_DIR+"/"+EXP_NAME+"_Pof"+var['param']+"_"+"_z="+str(zs)+"_allLoS.txt"                 
-            numpy.savetxt(outputfile, sub_pdf) 
-            print "Magnifier: saved all LoS PDFs to",outputfile
-                                                      
-            # --------------------------------------------------------------------
-            # Select only lightcones within certain number density limits
-            # Need to mask out the outliers
-            
-            means, fieldname = [], []
-            
-            for i in range(len(density)):
-                sub_pdf = [] 
+                print "Magnifier: %s - sampling %i LoS with number density ~ %.2f the average, mean mu=%.2f" % (field_name[i], Nlos, density[i], sub_mean)
                 
-                for j in xrange(len(new_pdf)):
-                    if density[i] - drange <= round(new_density[j],2)  <= density[i] + drange:
-                        sub_pdf.append(new_pdf[j])
+                if var['param']=='Mu':
+                    numpy.savetxt(CALIB_DIR+"/"+EXP_NAME+str(field_name[i])+"_PofMu.txt", sub_pdf)             
                 
-                sub_pdf = numpy.array(sub_pdf)
-                Nlos = len(sub_pdf)
-                if Nlos == 0:
-                    print "Magnifier: %s - there are NO LoS with number density ~ %.2f the average" % (field_name[i], density[i])    
-                    print "Magnifier: %s - no PDF will be made for this field" % (field_name[i])    
-                
-                else:
- 
-                    sub_mean = numpy.mean(sub_pdf) 
-                                
-                    print "Magnifier: %s - sampling %i LoS with number density ~ %.2f the average, mean mu=%.2f" % (field_name[i], Nlos, density[i], sub_mean)
-                    
-                    if var['param']=='Mu':
-                        numpy.savetxt(CALIB_DIR+"/"+EXP_NAME+str(field_name[i])+"_PofMu.txt", sub_pdf)             
-                    
-                    means.append(sub_mean)
-                    fieldname.append(field_name[i])
+                means.append(sub_mean)
+                fieldname.append(field_name[i])
 
-            meanmu_table = numpy.array([fieldname, means]).T
-            ascii.write(meanmu_table, "figs/borg_remove1815/borg_pdf_table_means.txt", names=['#field','mean_mu'])
+        meanmu_table = numpy.array([fieldname, means]).T
+        ascii.write(meanmu_table, CALIB_DIR+"/"+EXP_NAME+"_table_meanmu.txt", names=['#field','mean_mu'])
 
-            print "Mean mu of all the fields = ",numpy.mean(means)        
-            print "Magnifier: saved PDFs to",outputfile 
+        print "           Mean mu of all the fields = ",numpy.mean(means)        
+        print "Magnifier: saved PDFs to",outputfile 
             
     print pangloss.doubledashedline
 
