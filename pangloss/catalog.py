df41a170d30c66b3b8f64dee12453c4a1ab76819
Spencer Everett <spencerweverett@gmail.com>
2015-06-29 17:19:00 -0700

    Began a catalog class that will be the superclass of both BackgroundCatalog and ForegroundCatalog. Given a filename and configuration file, it reads in the catalog data using pangloss.io.readCatalog(). The class can also write the catalog data to a file and eventually will be able to generate a map of world coordinates on the sky #35

---
+++ pangloss/catalog.py
@@ -0,0 +1,67 @@
+import numpy as np
+import scipi as sp
+import matplotlib as plt
+import os
+from pangloss import io, config
+from astropy.table import Table, Column
+import astropy.wcs
+
+
+# ============================================================================
+
+class Catalog(object):
+    """
+    NAME
+        Catalog
+
+    PURPOSE
+        ???
+
+    COMMENTS
+        ???
+
+    INITIALISATION
+        ???
+            
+    METHODS
+        ???
+
+    BUGS
+        ???
+
+    AUTHORS
+      This file is part of the Pangloss project, distributed under the
+      GPL v2, by Tom Collett (IoA) and  Phil Marshall (Oxford). 
+      Please cite: Collett et al 2013, http://arxiv.org/abs/1303.6564
+
+    HISTORY
+      2015-06-29  Everett (SLAC)
+    """
+    def __init__(self,filename,configfile):        
+        self.filename = filename        
+        # Structures catalog metadata from configfile and reads in the catalog data
+        self.config = config.Configuration(configfile)
+        self.read(filename,config)
+    
+    def read(self,filename,config):    
+        # Uses astropy.table to read catalog, but with a few specific changes
+        self.data = io.readCatalog(filename,config)
+    
+    def write(self,output=os.getcwd()):
+        # Writes catalog data to current directory unless otherwise specified
+        self.data.write(output,format = 'ascii')
+    
+    def generate(self):
+        '''
+        Draw world-coordinates in the sky in RA and DEC
+        '''
+        # Will probably need to rewrite - I don't know how the catalog data is actually formatted yet
+        self.xmax = self.data['nRA'].max()
+        self.xmin = self.data['nRA'].min()
+        self.ymax = self.data['Dec'].max()
+        self.ymin = self.data['Dec'].min()
+        
+        # Use previous values to calculate necessary axes and plot in rads
+        
+        
+    
