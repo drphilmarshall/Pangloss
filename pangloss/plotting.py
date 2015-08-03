13e536e8de7fa65586289091014bda61ae82bcf7
Spencer Everett <spencerweverett@gmail.com>
2015-08-03 16:22:11 -0700

    Fixed the error propagationfor `xi_cross` and `xi_cross^prime` in the `plot_corr()` method of  `plotting.py`.

--- pangloss/plotting.py
+++ pangloss/plotting.py
@@ -99,14 +99,14 @@ def make_axes(fig,subplot,imsubplot=[0,1,0,1]):
     contained in the current figure instance, and also returned for
     ease of use.
     '''
-    
+
     # Make image axes for plotting maps in:
     imshow = fig.add_axes(viewport,label='imshow')
     imshow.set_xlim(imsubplot[0],imsubplot[1])
     imshow.set_ylim(imsubplot[2],imsubplot[3])
     imshow.get_xaxis().set_visible(False)
     imshow.get_yaxis().set_visible(False)
-    
+
     # Make wcs axes for plotting catalogs in:
     world = fig.add_axes(viewport,label='world')
     world.set_xlabel('Right Ascension / deg')
@@ -137,44 +137,44 @@ def plot_ellipse(ra,dec,size,mod,phi,axis,color,alpha):
     '''
     q = (1-mod)/(1+mod)
     ellipse = Ellipse(xy=[ra,dec],width=size,height=np.sqrt(q)*size,angle=phi)
-    axis.add_artist(ellipse)      
+    axis.add_artist(ellipse)
     ellipse.set_clip_box(axis.bbox)
     ellipse.set_alpha(alpha)
     ellipse.set_facecolor(color)
-    
+
     return
-    
+
 
 def plot_sticks(ra,dec,mod,phi,axis,color):
     '''
     Write docstring
     '''
-    
+
     # Make sure that all of the columns are the same length
     assert np.size(ra) == np.size(dec) and np.size(dec) == np.size(phi)
-    N = np.size(ra)  
-    
+    N = np.size(ra)
+
     # If N is 1, convert inputs into lists
     if N == 1:
         ra = np.array([ra])
         dec = np.array([dec])
         mod = np.array([mod])
         phi = np.array([phi])
-    
+
     # Convert the angles phi to rad
-    phi = np.deg2rad(phi)  
+    phi = np.deg2rad(phi)
 
     # Preallocation
     pt1 = np.zeros(N,dtype=tuple)
     pt2 = np.zeros(N,dtype=tuple)
     lines = np.zeros(N,dtype=tuple)
-    
+
     # Set scale size of sticks
     xi, xf = axis.get_xlim()
     yi, yf = axis.get_ylim()
     Lx, Ly = abs(xf-xi), abs(yf-yi)
     L = np.mean([Lx,Ly])
-    
+
     # Need this to see sticks (weak lensing)
     scale = 1.0
     size = scale*mod*L
@@ -187,16 +187,16 @@ def plot_sticks(ra,dec,mod,phi,axis,color):
         #pt1[i] = (ra[i]-s*np.cos(phi[i]), dec[i]-s*np.sin(phi[i]))
         #pt2[i] = (ra[i]+s*np.cos(phi[i]), dec[i]+s*np.sin(phi[i]))
         lines[i] = tuple([pt1[i],pt2[i]])
-    
+
     # Turn the array of lines into tuples
     lines = tuple(lines)
-    
+
     # Create the collection of sticks from these lines, and add them to the inputted axis
     sticks = LineCollection(lines,linestyles='solid',color=color,lw=2)
     axis.add_collection(sticks)
-    
+
     return
-    
+
 # ----------------------------------------------------------------------------
 
 def plot_corr(corr,corr_type='gg',corr_comp='plus',sep_units='arcmin',lensed='map',color=None,fig_size=10,M=None,galaxy_count=None):
@@ -205,109 +205,109 @@ def plot_corr(corr,corr_type='gg',corr_comp='plus',sep_units='arcmin',lensed='ma
     By default the method will plot the lensed-by-map correlation, but can also plot without lensing
     (lensed='none') or with the lensed-by-halo correlation (lensed='halo').
     '''
-    
+
     # If no color is inputted, plot in black
     if color is None:
         color = 'black'
-    
+
     # Get current figure (or make one if it doesn't exist)
     fig = plt.gcf()
-    
+
     if fig._label != 'Correlation':
-    
+
         # Set the figure name
         fig._label = 'Correlation'
-    
+
         # Set max and min dtheta
         min_sep = corr.min_sep
         max_sep = corr.max_sep
-        #min_sep = np.deg2rad(0.1/60.0)        
-        #max_sep = min_sep * 100.0 # 10 arcmin        
+        #min_sep = np.deg2rad(0.1/60.0)
+        #max_sep = min_sep * 100.0 # 10 arcmin
         if sep_units == 'arcmin':
             min_sep = np.rad2deg(min_sep)*60
             max_sep = np.rad2deg(max_sep)*60
-            
+
         elif sep_units == 'deg':
             min_sep = np.rad2deg(min_sep)
             max_sep = np.rad2deg(max_sep)
-        
+
         elif sep_units == 'rad':
             min_sep = min_sep
             max_sep = max_sep
-            
+
         # Set figure size
         plt.gcf().set_size_inches(fig_size,fig_size)
-            
+
         # Set plot settings
         plt.xscale('log')
         plt.gca().set_xlim(min_sep,max_sep)
-        
+
         # Set axes labels
         plt.xlabel(r'$\Delta\theta$ (arcmin)',fontsize=20)
-        
-        if corr_type == 'gg': plt.ylabel(r'Ellipticity-Ellipticity Correlation $\xi(\Delta\theta)$',fontsize=20)            
+
+        if corr_type == 'gg': plt.ylabel(r'Ellipticity-Ellipticity Correlation $\xi(\Delta\theta)$',fontsize=20)
         elif corr_type == 'ng': plt.ylabel(r'Galaxy-Mass Correlation $\xi(\Delta\theta)$',fontsize=20)
         else: pass # Can incorporate other correlation functions later if needed
-        
+
         # Plot xi=0 for reference
         plt.plot([min(np.exp(corr.logr)),max(np.exp(corr.logr))],[0,0],c='k',linestyle='dashed',linewidth=2)
 
         # Make axis ticks larger
         plt.gca().tick_params('both', length=5, width=1, which='major')
         plt.gca().tick_params('both', length=5, width=1, which='minor')
-    
+
     if corr_type == 'gg':
         # For shear-shear (or ellipticity-ellipticity) correlation
-    
+
         # Mark first label as intrinsic, observed, or predicted
-        if lensed == 'none': label1 = 'Intrinsic '            
-        if lensed == 'map': label1 = 'Observed '        
+        if lensed == 'none': label1 = 'Intrinsic '
+        if lensed == 'map': label1 = 'Observed '
         elif lensed == 'halo': label1 = 'Predicted '
-            
+
         # Create the correct component label and linestyle to be used in legend
-        if corr_comp == 'plus': 
+        if corr_comp == 'plus':
             # Plot xi_+ with a solid line
             correlation = corr.xip
             err = np.sqrt(corr.varxi)
             ls = '-'
-            label2 = '+'           
-            
+            label2 = '+'
+
         elif corr_comp == 'minus':
             # Plot xi_- with a dotted line
-            correlation = corr.xim    
+            correlation = corr.xim
             err = np.sqrt(corr.varxi)
             ls = ':'
             label2 = '-'
-            
+
         elif corr_comp == 'cross':
             # Plot xi_x with a dashed line
             correlation = 0.5*(corr.xim_im-corr.xip_im)
-            err = 0.5*np.sqrt(2.0*corr.varxi**2)
+            err = 0.5*np.sqrt(2.0*corr.varxi)
             ls = '--'
-            label2 = r'\times'         
-            
+            label2 = r'\times'
+
         elif corr_comp == 'cross_prime':
             # Plot xi_x prime with a dot-dash line
             correlation = corr.xip_im
-            err = 0.5*np.sqrt(2.0*corr.varxi**2)
+            err = np.sqrt(corr.varxi)
             ls = '-.'
-            label2 = r'\times^\prime'       
-            
+            label2 = r'\times^\prime'
+
         # If the multiplicative error M is not None, add it to the legend label
         if M is not None:
             label2 += r',\,M={}'.format(M)
-        
+
         # Plot the inputted shear-shear (or ellipticity-ellipticity) correlation function component
         plt.errorbar(np.exp(corr.logr),correlation,err,c=color,linestyle=ls,label=label1+r'$\xi_'+label2+'$',linewidth=2)
-        
+
     elif corr_type == 'ng':
         # Plot the galaxy-mass correlation function (xi_gm)
 
         # Mark first label as intrinsic, observed, or predicted
-        if lensed == 'none': label1 = 'Intrinsic '            
-        if lensed == 'map': label1 = 'Observed '        
+        if lensed == 'none': label1 = 'Intrinsic '
+        if lensed == 'map': label1 = 'Observed '
         elif lensed == 'halo': label1 = 'Predicted '
-        
+
         if corr_comp == 'real':
             # Plot real(xi) with a solid line
             correlation = corr.xi
@@ -329,23 +329,23 @@ def plot_corr(corr,corr_type='gg',corr_comp='plus',sep_units='arcmin',lensed='ma
 
         # Plot the inputted galaxy-galaxy correlation function component
         plt.errorbar(np.exp(corr.logr),correlation,err,c=color,linestyle=ls,label=label1+label2+r'$\left(\xi\right)$',linewidth=2)
-    
+
     else:
         # Can incorporate other correlation functions here if needed
         pass
-    
+
     # get legend handles
     handles, labels = plt.gca().get_legend_handles_labels()
     # remove the errorbars
     handles = [h[0] for h in handles]
     plt.legend(handles,labels,fontsize=18)
-    
+
     if galaxy_count is not None:
         # place a text box in upper left in axes coords
         plt.gca().text(0.1,0.95,str(galaxy_count)+' background galaxies used',transform=plt.gca().transAxes,fontsize=18,verticalalignment='top')
-    
+
     return
-    
+
 #-------------------------------------------------------------------------------------------------------------------
 # This section is for code only used to create demo plots. None of these are currently used for actual pangloss use.
 
@@ -358,29 +358,29 @@ def calc_corr_demo(units='deg'):
     This is only used for demonstrations to show the connection between the difference in galaxy
     orientation and the different correlation function values.
     '''
-    
+
     if units == 'deg':
         # Colormap locations
         i = np.arange(-20,135+25,.1)
         j = np.arange(-20,135+25,.1)
-        
+
         # Create grid
         I,J = np.meshgrid(i,j)
-        
+
         # Calculate shear components
         gt_i = -np.cos(np.deg2rad(2*I))
         gt_j = -np.cos(np.deg2rad(2*(I+J)))
         gx_i = -np.sin(np.deg2rad(2*I))
         gx_j = -np.sin(np.deg2rad(2*(I+J)))
-        
+
     elif units == 'rad':
         # Colormap locations
         i = np.arange(-0.35,np.pi+0.35,.01)
         j = np.arange(-0.35,np.pi+0.35,.01)
-        
+
         # Create grid
         I,J = np.meshgrid(i,j)
-        
+
         # Calculate shear components
         gt_i = -np.cos(2*I)
         gt_j = -np.cos(2*(I+J))
@@ -398,17 +398,17 @@ def calc_corr_demo(units='deg'):
     xi_m = gtt-gxx
     xi_x = gtx
     xi_xp = gtx-gxt
-    
+
     return xi_p,xi_m,xi_x,xi_xp
-    
+
 
 def plot_corr_demo(corr,corr_type='plus',units='deg'):
     '''
     Plots the inputted correlation function type as a color map with the ellipticity sticks at their
-    corresponding orientations overlaid. This is only used for demonstrations to show the connection 
+    corresponding orientations overlaid. This is only used for demonstrations to show the connection
     between the difference in galaxy orientation and the different correlation function values.
     '''
-    
+
     if units == 'deg':
         # Colormap locations
         i = np.arange(-20,135+25,.1)
@@ -416,14 +416,14 @@ def plot_corr_demo(corr,corr_type='plus',units='deg'):
         # Stick locations
         x = np.arange(0,180,45)
         y = np.arange(0,180,45)
-        
+
         # Create grid
         I,J = np.meshgrid(i,j)
         X,Y = np.meshgrid(x,y)
-        
+
         # Stick offset
         dx = 7
-        
+
     elif units == 'rad':
         # Colormap locations
         i = np.arange(-0.35,np.pi+0.35,.01)
@@ -431,14 +431,14 @@ def plot_corr_demo(corr,corr_type='plus',units='deg'):
         # Stick locations
         x = np.arange(0,np.pi,np.pi/4)
         y = np.arange(0,np.pi,np.pi/4)
-        
+
         # Create grid
         I,J = np.meshgrid(i,j)
         X,Y = np.meshgrid(x,y)
-        
+
         # Stick offset
         dx = 0.125
-        
+
     # Create plot
     plt.imshow(corr,origin='lower',cmap='bwr',extent=(np.min(I), np.max(I), np.min(J), np.max(J)))
     plt.xlabel(r'$\theta_i\,(degree)$',fontsize=20)
@@ -446,13 +446,13 @@ def plot_corr_demo(corr,corr_type='plus',units='deg'):
     plt.gcf().set_size_inches(10,10)
     plt.colorbar()
     plt.grid()
-    
+
     # Set title
     if corr_type == 'plus': plt.title(r'$\xi_+$',fontsize=25)
     elif corr_type == 'minus': plt.title(r'$\xi_-$',fontsize=25)
     elif corr_type == 'cross': plt.title(r'$\xi_\times$',fontsize=25)
     elif corr_type == 'crossp': plt.title(r'$\xi^\prime_\times$',fontsize=25)
-    
+
     # Get axes and set stick modulus
     ax = plt.gca()
     mod = np.ones(len(I[0,:]))/20.0
@@ -488,25 +488,25 @@ def plot_corr_demo(corr,corr_type='plus',units='deg'):
     # Set axis limits
     plt.xlim([np.min(I),np.max(I)])
     plt.ylim([np.min(J),np.max(J)])
-    
+
     return
-    
+
 # ----------------------------------------------------------------------------
 # Plot color-coded galaxies around a lens and a scatter plot of the correlation coefficients using
 # the same color scheme. Used in VisualizingCorrelationFunction.ipyn.
-    
+
 def plot_lensed_colors(B,subplot=[1.1175,1.0925,-1.54,-1.5175],center=[1.10425,-1.52875],fig_size=10):
     '''
     Plot the background galaxies in the catalog B contained in the inputted subplot in different colors
     as a function of their distance from the inputted center.
     '''
-    
+
     # Create new figure with correct axes
     fig = plt.gcf()
     pangloss.plotting.make_axes(fig,subplot=[1.1175,1.0925,-1.54,-1.5175])
     ax = plt.gca()
-    
-    # Extract data from galaxies contained in subplot 
+
+    # Extract data from galaxies contained in subplot
     galaxies = B.return_galaxies(ra_lim=[subplot[0],subplot[1]],dec_lim=[subplot[2],subplot[3]])
     ra = np.rad2deg(galaxies['RA'])
     dec = np.rad2deg(galaxies['Dec'])
@@ -527,7 +527,7 @@ def plot_lensed_colors(B,subplot=[1.1175,1.0925,-1.54,-1.5175],center=[1.10425,-
     for i in range(np.size(ra)):
         # Don't plot if galaxy is strongly lensed
         if galaxies['strong_flag'][i] == 1: continue
-            
+
         # Calculate galaxy distance to center
         dra_c = abs(ra[i]-ra_c)
         ddec_c = abs(dec[i]-dec_c)
@@ -544,14 +544,14 @@ def plot_lensed_colors(B,subplot=[1.1175,1.0925,-1.54,-1.5175],center=[1.10425,-
         # Plot galaxy ellipticity
         #plt.subplot(1,5,1)
         pangloss.plot_sticks(ra[i],dec[i],mod[i],-phi[i],ax,color=color)
-        
+
     # Plot lens center
     #plt.subplot(1,5,1)
     plt.scatter(1.10425,-1.52875,s=200)
-    
+
     # Set figure size
     fig.set_size_inches(fig_size,fig_size)
-    
+
     # Add scale bar
     Lx, Ly = abs(subplot[0]-subplot[1]), abs(subplot[2]-subplot[3])
     L = np.mean([Lx,Ly])
@@ -559,17 +559,17 @@ def plot_lensed_colors(B,subplot=[1.1175,1.0925,-1.54,-1.5175],center=[1.10425,-
     bar.size_bar._children[0]._linewidth = 2
     #bar.size_bar._children[0]._edgecolor = (1,0,0,1)
     ax.add_artist(bar)
-        
+
     plt.show()
-    
+
     return
-    
-    
+
+
 def calc_corr_components(points):
     '''
     Calculate the plus, minus, cross, and cross^prime components of the correlation between each galaxy pair.
     '''
-    
+
     del_xi_p = []
     del_xi_m = []
     del_xi_x = []
@@ -581,7 +581,7 @@ def calc_corr_components(points):
     # Center of lens
     ra_c = 1.10425
     dec_c = -1.52875
-    
+
     # Distance bin cutoffs
     c1 = 0.0025
     c2 = 0.005
@@ -624,7 +624,7 @@ def calc_corr_components(points):
                 elif (r1_c >= c2) and (r1_c < c3) and (r2_c >= c2) and (r2_c < c3): c.append((1,1,0,0.6))
                 elif (r1_c >= c3) and (r1_c < c4) and (r2_c >= c3) and (r2_c < c4): c.append((0,1,0,0.4))
                 elif (r1_c >= c4) and (r1_c < c5) and (r2_c >= c4) and (r2_c < c5): c.append((0,0,1,0.2))
-                else: c.append((0.5,0,0.5,0.01))           
+                else: c.append((0.5,0,0.5,0.01))
 
                 # Calculate shear components
                 g1t = -(el1*np.e**(-2j*phi)).real
@@ -644,18 +644,18 @@ def calc_corr_components(points):
                 del_xi_m.append(del_tt-del_xx)
                 del_xi_x.append(del_tx)
                 del_xi_xp.append(del_xt)
-                
+
     return r,del_xi_p,del_xi_m,del_xi_x,del_xi_xp,c
-            
-    
+
+
 def plot_corr_component(r,corr,corr_type,c):
     '''
     Plot a single correlation component.
     '''
-    
+
     # Create another subplot
     #plt.subplot(1,5,i)
-    
+
     plt.scatter(np.array(r)*60.0,corr,color=c)
     plt.plot([min(60.0*np.array(r)),max(60.0*np.array(r))],[0,0],'--k')
     plt.gcf().set_size_inches(10,10)
@@ -664,7 +664,7 @@ def plot_corr_component(r,corr,corr_type,c):
     plt.xscale('log')
 
     plt.xlabel(r'$\Delta\theta$ (arcmin)',fontsize=20)
-    
+
     if corr_type == 'plus':
         plt.ylabel(r'$\delta\xi_+(\theta)$',fontsize=20)
     elif corr_type == 'minus':
@@ -673,31 +673,31 @@ def plot_corr_component(r,corr,corr_type,c):
         plt.ylabel(r'$\delta\xi_\times(\theta)$',fontsize=20)
     elif corr_type == 'cross_prime':
         plt.ylabel(r'$\delta\xi_\times^\prime(\theta)$',fontsize=20)
-        
-        
+
+
     # Make axis ticks larger
     plt.gca().tick_params('both', length=5, width=1, which='major')
     plt.gca().tick_params('both', length=5, width=1, which='minor')
-        
+
     plt.show()
-    
+
     return
-    
-    
+
+
 def plot_corr_color_demo(N=200):
     '''
     Runs the correlation color demo using the plot_lens_colors(), calc_corr_components(),
     and plot_corr_component() methods.
     '''
-    
+
     # Load in the (0,0) Kappamap and Shearmap
     K = pangloss.Kappamap(PANGLOSS_DIR+'/data/GGL_los_8_0_0_N_4096_ang_4_rays_to_plane_37_f.kappa',FITS=False)
-    S = pangloss.Shearmap([PANGLOSS_DIR+'/data/GGL_los_8_0_0_N_4096_ang_4_rays_to_plane_37_f.gamma_1',PANGLOSS_DIR+'/data/GGL_los_8_0_0_N_4096_ang_4_rays_to_plane_37_f.gamma_2'],FITS=False)    
-    
+    S = pangloss.Shearmap([PANGLOSS_DIR+'/data/GGL_los_8_0_0_N_4096_ang_4_rays_to_plane_37_f.gamma_1',PANGLOSS_DIR+'/data/GGL_los_8_0_0_N_4096_ang_4_rays_to_plane_37_f.gamma_2'],FITS=False)
+
     # Create a background catalog and lens the galaxies
     B = pangloss.BackgroundCatalog(N=N,sigma_e=0.01,domain=[1.1175,1.0925,-1.54,-1.515],field=[0,0,0,0]) # High ellipticity used to highlight the intrinsic shape of background sources
     B.lens_by_map(K,S)
-    
+
     # Plot background catalog with color scheme based upon galaxy distance from lens
     plot_lensed_colors(B)
 
@@ -714,7 +714,7 @@ def plot_corr_color_demo(N=200):
     for i in range(len(ra)):
         # Don't calculate correlation for strongly-lensed sources
         if B.galaxies['strong_flag'][i] == 1: continue
-            
+
         r = ra[i]
         d = dec[i]
         e = el[i]
@@ -722,19 +722,19 @@ def plot_corr_color_demo(N=200):
 
     # Calculate the correlation components between galaxies
     r,del_xi_p,del_xi_m,del_xi_x,del_xi_xp,c = calc_corr_components(points)
-    
+
     # Calculate the shear-shear correlation function
     #gg=B.calculate_corr()
-    
-    # 
-    plot_corr_component(r,del_xi_p,'plus',c)  
+
+    #
+    plot_corr_component(r,del_xi_p,'plus',c)
     plot_corr_component(r,del_xi_m,'minus',c)
-    plot_corr_component(r,del_xi_x,'cross',c)  
-    plot_corr_component(r,del_xi_xp,'cross_prime',c)  
-    
+    plot_corr_component(r,del_xi_x,'cross',c)
+    plot_corr_component(r,del_xi_xp,'cross_prime',c)
+
     # Set figure size for figure containing all subplots
     #plt.gcf().set_size_inches(50,10)
-   
+
     return r,del_xi_p,del_xi_m,del_xi_x,del_xi_xp,c
-    
+
 #-------------------------------------------------------------------------------------------------------------------
