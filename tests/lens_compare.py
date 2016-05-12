0858a5e40353a200ab44054f3a73eccee61b2b79
Spencer Everett <spencerweverett@gmail.com>
2016-05-12 15:47:11 -0500

    More `kappamap` debugging.

--- tests/lens_compare.py
+++ tests/lens_compare.py
@@ -32,7 +32,7 @@ rel_compare = False
 smooth_compare = False
 
 # Turn on for plotting maps
-maps = True
+maps = False
 
 # Pangloss:
 PANGLOSS_DIR = os.path.expandvars("$PANGLOSS_DIR")
@@ -56,10 +56,10 @@ if vb is True: print('Generating background catalog...')
 #d = [1.85,1.15,-1.85,-1.15]
 #d = [1.75,1.25,-1.75,-1.25]
 #d = [1.65,1.35,-1.65,-1.35]
-#d = [1.6,1.4,-1.6,-1.4] # 1440 galaxies
+d = [1.6,1.4,-1.6,-1.4] # 1440 galaxies
 #d = [1.55,1.45,-1.55,-1.45]
 #d = [1.55,1.48,-1.55,-1.48] # 176 galaxies
-d = [1.55,1.52,-1.61,-1.59] # only galaxies in subplot
+#d = [1.55,1.52,-1.61,-1.59] # only galaxies in subplot
 #d = [1.55,1.54,-1.61,-1.6] # ~3 galaxies
 B = pangloss.BackgroundCatalog(N=10.0,sigma_e=0.01,domain=d,field=[0,0,0,0])
 
@@ -70,7 +70,7 @@ print 'Background catalog has',B.galaxy_count,'galaxies'
 
 # Drill the lightcones
 if vb is True: print('Drilling lightcones...')
-lc_radius = 6.0
+lc_radius = 8.0
 B.drill_lightcones(radius=lc_radius,foreground=F,save=False)
 
 # Save copy of background lightcones for relevant halos:
