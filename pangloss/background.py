Altered previous methods related to `bin_to_maps()` so that the binned maps now plot in correct wcs units. Added the `compare_binned_maps()` method to `plotting.py`, which is close to being able to plot the three maps simultaneously. Simply using the `kappamap` plotting method overwrites previous maps at the moment. #68 #96

pangloss/background.py
@@ -675,9 +675,7 @@ class BackgroundCatalog(pangloss.Catalog):
                 NX = int( np.rad2deg(ramax-ramin)*60.0/(binsize*np.cos(np.deg2rad(np.mean(dec)))) )
 
                 rabins = np.linspace(ramin,ramax,NX+1)
-                # x = np.outer(np.ones(NY),rabins)
                 decbins = np.linspace(decmin,decmax,NY+1)
-                # y = np.outer(decbins,np.ones(NX))
 
                 # Set up some data arrays:
                 empty = np.outer(np.zeros(NY),np.zeros(NX))
@@ -727,21 +725,24 @@ class BackgroundCatalog(pangloss.Catalog):
 
         # Store these histograms in WLMap objects
         map_xy = [self.map_x, self.map_y]
-        kappamap = pangloss.Kappamap(data=[kappadata,x,y,self.domain,map_xy])
-        shearmap = pangloss.Shearmap(data=[gammadata,x,y,self.domain,map_xy])
+        kappamap = pangloss.Kappamap(data=[kappadata,self.domain,map_xy])
+        shearmap = pangloss.Shearmap(data=[gammadata,self.domain,map_xy])
+        #kappamap = pangloss.Kappamap(data=[kappadata,x,y,self.domain,map_xy]) # no longer use x,y
+        #shearmap = pangloss.Shearmap(data=[gammadata,x,y,self.domain,map_xy]) # no longer use x,y
 
         ## ONLY HERE FOR TESTING
-        subplot = [ramax,ramin,decmin,decmax] # ra flipped b/c left-handed
-        print subplot
+        subplot = np.rad2deg([ramax,ramin,decmin,decmax]) # ra flipped b/c left-handed
+        #subplot = [ramin,ramax,decmin,decmax]
+
         # Used for colormap scaling
-        Kmin = np.min(kappadata[0])
-        Kmax = np.max(kappadata[0])
-        plt.imshow(kappadata[0],cmap='gray_r',vmin=Kmin,vmax=Kmax,origin='lower')
-        #kappamap.plot(subplot=subplot,coords='physical')
+        #Kmin = np.min(kappadata[0])
+        #Kmax = np.max(kappadata[0])
+        #plt.imshow(kappadata[0],cmap='gray_r',vmin=Kmin,vmax=Kmax,origin='lower')
+        kappamap.plot(subplot=subplot,coords='world')
 
         if savefile is not None:
             plt.savefig(PANGLOSS_DIR+'/data/binned_maps/'+savefile, bbox_inches='tight')
-        plt.show()
+        #plt.show()
 
         return kappamap, shearmap
 

pangloss/wlmap.py
@@ -71,20 +71,22 @@ class WLMap:
             self.input = mapfiles
             self.origin = 'file'
 
-        # Make a WLMap from scratch, given 3 numpy arrays: d,x,y
+        # Make a WLMap from scratch, given 3 numpy arrays: d,domain,map_xy
         # d must be a 3D array, to allow for shear as well as kappa.
         # x and y must be meshgrids that specify the world coordinates
-        # of the pixel centers.
+        # of the pixel centers. domain =[ra_i, ra_f, dec_i, dec_f],
+        # map_xy = [map_x, map_y]. (no longer need x and y)
         elif data is not None:
             assert type(data) == list
-            assert len(data) == 5
-            d,x,y,domain,map_xy = data[0],data[1],data[2],data[3],data[4]
+            assert len(data) == 3
+            #d,x,y,domain,map_xy = data[0],data[1],data[2],data[3],data[4] # OLD - don't need x,y if domain is passed
+            d,domain,map_xy = data[0],data[1],data[2]
             assert len(d.shape) == 3
             assert len(domain) == 4
             assert len(map_xy) == 2
-            assert d[0].shape[0] == x.shape[0]-1
-            assert d[0].shape[1] == y.shape[0]-1
-            assert x.shape == y.shape # For now, must be square!
+            #assert d[0].shape[0] == x.shape[0]-1
+            #assert d[0].shape[1] == y.shape[0]-1
+            #assert x.shape == y.shape # For now, must be square!
             if len(d) == 2: assert d[0].shape == d[1].shape
             self.input = []
             self.origin = 'scratch'
@@ -131,7 +133,7 @@ class WLMap:
                 self.map_x.append(map_xy[0]) # The x location of the map grid
                 self.map_y.append(map_xy[1]) # The y location of the map grid
 
-            self.reformat_data(d,x,y,domain)
+            self.reformat_data(d,domain)
 
             # Set up the WCS:
             #self.make_wcs(x,y)
@@ -200,39 +202,44 @@ class WLMap:
 
 # ----------------------------------------------------------------------------
 
-    def reformat_data(self,d,x,y,domain):
+    def reformat_data(self,d,domain):
         #
         for i in range(0,len(d)):
             #
             self.values.append(d[i])
-            #self.NX.append(np.sqrt(self.values[i].shape[0]))
 
             ra_i, ra_f, dec_i, dec_f = domain[0], domain[1], domain[2], domain[3] # degrees
             dra, ddec = abs(ra_i-ra_f), abs(dec_i-dec_f) # degrees
             assert dra == ddec
-            #self.field.append(dra) # degrees
+            self.field.append(dra) # degrees
             #self.NX.append(int( 4096.0 * (dra / 4.0) ))
             self.NX.append(self.values[i].shape[0])
-            self.field.append(4.0) # deg (must match binary files!)
             self.PIXSCALE.append(self.field[i]/(1.0*self.NX[i])) # degrees
-            self.setwcs(i)
+            self.make_wcs(i,domain)
+            self.make_header(i)
             self.input.append('')
             self.output.append('')
-
-            #self.input.append('background_catalog_{}_{}'.format()
 
         return
 
 # ----------------------------------------------------------------------------
-
-    def make_wcs(self,x,y):
+    #def make_wcs(self,x,y):
+    def make_wcs(self,i,domain):
+        '''
+        Make wcs for inputted data
+        '''
+        ra_i, ra_f, dec_i, dec_f = domain[0], domain[1], domain[2], domain[3]
+        #ramin = np.min(np.min(x))
+        #ramax = np.max(np.max(x))
+        #decmin = np.min(np.min(y))
+        #decmax = np.max(np.max(y))
         self.wcs.append(dict())
         # ra  = CRVAL1 + CD1_1*(i-CRPIX1)
         # dec = CRVAL2 + CD2_2*(j-CRPIX2)
         self.wcs[i]['CRPIX1'] = 0.0
         self.wcs[i]['CRPIX2'] = 0.0
-        self.wcs[i]['CRVAL1'] =  0.5*self.field[i] + 0.5*self.PIXSCALE[i] - self.map_x[i]*self.field[i]
-        self.wcs[i]['CRVAL2'] = -0.5*self.field[i] + 0.5*self.PIXSCALE[i] + self.map_y[i]*self.field[i]
+        self.wcs[i]['CRVAL1'] =  ra_i
+        self.wcs[i]['CRVAL2'] = dec_i
         self.wcs[i]['CD1_1'] = -self.PIXSCALE[i]
         self.wcs[i]['CD1_2'] = 0.0
         self.wcs[i]['CD2_1'] = 0.0
@@ -240,12 +247,14 @@ class WLMap:
         self.wcs[i]['CTYPE1'] = 'RA---TAN'
         self.wcs[i]['CTYPE2'] = 'DEC--TAN'
 
+        '''
         # i = LTV1 + LTM1_1*(x/rad)
         # j = LTV2 + LTM2_2*(y/rad)
         self.wcs[i]['LTV1'] = 0.5*self.field[i]/self.PIXSCALE[i] - 0.5
         self.wcs[i]['LTV2'] = 0.5*self.field[i]/self.PIXSCALE[i] - 0.5
         self.wcs[i]['LTM1_1'] = 1.0/np.deg2rad(self.PIXSCALE[i])
         self.wcs[i]['LTM2_2'] = 1.0/np.deg2rad(self.PIXSCALE[i])
+        '''
         return
 
 # ----------------------------------------------------------------------------
@@ -260,8 +269,10 @@ class WLMap:
         # dec = CRVAL2 + CD2_2*(j-CRPIX2)
         self.wcs[i]['CRPIX1'] = 0.0
         self.wcs[i]['CRPIX2'] = 0.0
-        self.wcs[i]['CRVAL1'] =  0.5*self.field[i] + 0.5*self.PIXSCALE[i] - self.map_x[i]*self.field[i]
-        self.wcs[i]['CRVAL2'] = -0.5*self.field[i] + 0.5*self.PIXSCALE[i] + self.map_y[i]*self.field[i]
+        self.wcs[i]['CRVAL1'] =  0.5*self.field[i] - self.map_x[i]*self.field[i]
+        self.wcs[i]['CRVAL2'] = -0.5*self.field[i] + self.map_y[i]*self.field[i]
+        # self.wcs[i]['CRVAL1'] =  0.5*self.field[i] + 0.5*self.PIXSCALE[i] - self.map_x[i]*self.field[i]
+        # self.wcs[i]['CRVAL2'] = -0.5*self.field[i] + 0.5*self.PIXSCALE[i] + self.map_y[i]*self.field[i]
         self.wcs[i]['CD1_1'] = -self.PIXSCALE[i]
         self.wcs[i]['CD1_2'] = 0.0
         self.wcs[i]['CD2_1'] = 0.0
@@ -271,10 +282,12 @@ class WLMap:
 
         # i = LTV1 + LTM1_1*(x/rad)
         # j = LTV2 + LTM2_2*(y/rad)
+
         self.wcs[i]['LTV1'] = 0.5*self.field[i]/self.PIXSCALE[i] - 0.5
         self.wcs[i]['LTV2'] = 0.5*self.field[i]/self.PIXSCALE[i] - 0.5
         self.wcs[i]['LTM1_1'] = 1.0/np.deg2rad(self.PIXSCALE[i])
         self.wcs[i]['LTM2_2'] = 1.0/np.deg2rad(self.PIXSCALE[i])
+
         return None
 
 # ----------------------------------------------------------------------------
@@ -448,8 +461,10 @@ class WLMap:
             raise IOError('Error: Subplot bounds can only be in pixel, physical, or world coordinates.')
 
         # Convert subplot bounds to pixel values
+        print 'xi = {}, xf = {}'.format(xi,xf)
         pix_xi,pix_yi = self.world2image(xi,yi)
         pix_xf,pix_yf = self.world2image(xf,yf)
+        print 'pix_xi = {}, pix_xf = {}'.format(pix_xi,pix_xf)
 
         # Pixel length of subplot
         pix_Lx = pix_xf-pix_xi